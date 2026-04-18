3a1c1bd3d710f316383d610b3b4eea47245b12b3
bbecerikli <betulberrabe@gmail.com>
2026-04-19 01:33:25 +0300

    baslik_olusturuldu.

--- src/ui.py
+++ src/ui.py
@@ -1,4 +1,17 @@
 #TODO
 import customtkinter as ctk
+
 app = ctk.CTk()
+app.geometry("800x800")
+app.title("VNS: Akıllı Ses Deşifre Sistemi      ")
+ana_baslik = ctk.CTkLabel(
+    app, 
+    text="VNS SES ANALİZ SİSTEMİ", 
+    text_color="#708090", 
+    font=("Consolas", 15, "bold"),
+    width=600,
+    
+    anchor="center"
+)
+ana_baslik.pack(pady=15)
 app.mainloop()
